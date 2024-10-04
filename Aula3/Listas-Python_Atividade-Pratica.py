201aa20ba594540a17c0eff219a494e6b9929d35
Ricardo Hideki Tsugikava Junior <166323985+DekyDeky@users.noreply.github.com>
2024-10-04 19:56:16 -0300

    Create Listas-Python_Atividade-Pratica.py

---
+++ Aula3/Listas-Python_Atividade-Pratica.py
@@ -0,0 +1,25 @@
+# Crie uma lista vazia chamada numeros
+
+numeros = []
+
+# Adicione os números 5, 10, 15 à lista usando o método append()
+
+numeros.append(5)
+
+numeros.append(10)
+
+numeros.append(15)
+
+print(numeros)
+
+numeros.insert(1, 20)
+
+print(numeros)
+
+numeros.remove(10)
+
+print(numeros)
+
+numeros.sort()
+
+print(numeros)
