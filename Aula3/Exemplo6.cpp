7a7f96ee86bb8012692532bd291d269f84aca7ee
Ricardo Hideki Tsugikava Junior <166323985+DekyDeky@users.noreply.github.com>
2024-10-04 19:50:25 -0300

    Create Exemplo6.cpp

---
+++ Aula3/Exemplo6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float nota1, nota2, media;
+
+    cout << "Digite a primeira nota: ";
+
+    cin >> nota1;
+
+    cout << "Digite a segunda nota: ";
+
+    cin >> nota2;
+
+    media = (nota1 + nota2) / 2;
+
+    cout << media;
+
+    return 0;
+
+}
